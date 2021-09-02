2dfe3a2f4abd75394eb08f54f61f72de4cbfd438
Camyla Romão <camyromao@gmail.com>
2021-09-02 15:33:39 -0300

    add test cases for game_over method

--- hangman/spec/lib/game_spec.rb
+++ hangman/spec/lib/game_spec.rb
@@ -13,5 +13,18 @@ RSpec.describe Game do
             expect(g.won?).to eq false
         end
     end
+    describe "#game_over" do
+        it "returns true if chances are over" do
+            g = Game.new()
+            ["q", "w", "e", "t", "g"].each { |l| g.guess(l) }
+            expect(g.game_over?).to eq true
+        end
+
+        it "returns false if chances are not over" do
+            g = Game.new()
+            ["c", "a", "m", "y", "l"].each { |l| g.guess(l) }
+            expect(g.game_over?).to eq false
+        end
+    end
     end
 end
